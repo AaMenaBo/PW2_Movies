0e481e3f8b52da1d20c85163db30387320c59a40
Aaron Mena <aamenabo@est.utn.ac.cr>
2024-08-03 14:31:37 -0600

    Se crean las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\StudioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+
+Route::prefix('movies')->group(function () { //Se puede agregar el auth despues 
+    //Rutas de vistas
+    Route::get('/create', [MovieController::class, 'create'])->name('movies.create');
+    Route::get('/{movie}/edit', [MovieController::class, 'edit'])->name('movies.edit');
+
+    //Rutas de Metodos
+    Route::post('/store', [MovieController::class, 'store'])->name('movies.store');
+    Route::put('/{movie}', [MovieController::class, 'update'])->name('movies.update');
+    Route::delete('/{movie}', [MovieController::class, 'destroy'])->name('movies.destroy');
+});
+
+Route::prefix('category')->group(function () {
+    Route::get('/{category}', [MovieController::class, 'viewByCategory'])->name('movies.genre');
+});
+Route::prefix('studio')->group(function () {
+    Route::get('/{studio}', [MovieController::class, 'viewBystudio'])->name('movies.studio');
 });

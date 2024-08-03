26cdbd19b82f6ccea0e3fbd73a083ffe8c97d974
Aaron Mena <aamenabo@est.utn.ac.cr>
2024-08-03 13:34:02 -0600

    Cambios en modelos Se crearon accesores y mutadores para mantener integridad

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,18 @@ class Movie extends Model
     public function Studio()
     {
         return $this->belongsTo(Studio::class);
+    }
+    public function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucfirst($value),
+            set: fn (string $value) => strtolower($value)
+        );
+    }
+    protected function casts(): array
+    {
+        return [
+            'release_date' => 'datetime:Y-m-d',
+        ];
     }
 }
--- app/Models/Studio.php
+++ app/Models/Studio.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,11 @@ class Studio extends Model
     public function Movies()
     {
         return $this->hasMany(Movie::class);
+    }
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucfirst($value),
+        );
     }
 }
